48602580fc28d1eb7b0319914b3d8d74eb615051
Demetr Sin <sonofgod1551@gmail.com>
2023-12-12 19:12:03 +0200

    New

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI, Request
+import uvicorn
+from fastapi import FastAPI, Request, Form, BackgroundTasks
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel, EmailStr, constr, field_validator
+
+import send_mail
 
 app = FastAPI(debug=True)
 
@@ -24,6 +27,11 @@ class ContactForm(BaseModel):
             raise ValueError("Phone number must be exactly 10 digits.")
         return value
 
+    @field_validator("name")
+    def validate_name(cls, value):
+        if not type(value, str):
+            raise ValueError("Name must be string")
+        return value
 @app.get('/', response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse('base.html', {'request': request})
@@ -44,5 +52,19 @@ async def contact(request: Request):
     return templates.TemplateResponse('contact.html', {'request': request})
 
 
-
-
+# @app.get('/send-email/asynchronous')
+# async def send_email_asynchronous():
+#     await send_mail.send_email_async('Hello World','someemail@gmail.com',
+#     {'title': 'Hello World', 'name': 'John Doe'})
+#     return 'Success'
+#
+#
+# @app.get('/send-email/backgroundtasks')
+# def send_email_backgroundtasks(background_tasks: BackgroundTasks):
+#     send_mail.send_email_background(background_tasks, 'Hello World',
+#     'someemail@gmail.com', {'title': 'Hello World', 'name':       'John Doe'})
+#     return 'Success'
+#
+#
+# if __name__ == '__main__':
+#     uvicorn.run('main:app', reload=True)
