0db6e0658e58a9cd027f5c2467e9e44e17eaf022
Demetr Sin <sonofgod1551@gmail.com>
2023-12-16 19:55:33 +0200

    almost done 17/12/23

--- send_mail.py
+++ send_mail.py
@@ -1,56 +1,53 @@
-# import os
-# from fastapi import BackgroundTasks
-# from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
-#
-# from dotenv import load_dotenv
-#
-# load_dotenv('.env')
-#
-#
-# class Envs:
-#     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-#     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-#     MAIL_FROM = os.getenv('MAIL_FROM')
-#     MAIL_PORT = int(os.getenv('MAIL_PORT'))
-#     MAIL_SERVER = os.getenv('MAIL_SERVER')
-#     MAIL_FROM_NAME = os.getenv('MAIN_FROM_NAME')
-#
-#
-# conf = ConnectionConfig(
-#     MAIL_USERNAME=Envs.MAIL_USERNAME,
-#     MAIL_PASSWORD=Envs.MAIL_PASSWORD,
-#     MAIL_FROM=Envs.MAIL_FROM,
-#     MAIL_PORT=Envs.MAIL_PORT,
-#     MAIL_SERVER=Envs.MAIL_SERVER,
-#     MAIL_FROM_NAME=Envs.MAIL_FROM_NAME,
-#     MAIL_TLS=True,
-#     MAIL_SSL=False,
-#     USE_CREDENTIALS=True,
-#     TEMPLATE_FOLDER='./templates/email'
-# )
-#
-#
-# async def send_email_async(subject: str, email_to: str, body: dict):
-#     message = MessageSchema(
-#         subject=subject,
-#         recipients=[email_to],
-#         body=body,
-#         subtype='html',
-#     )
-#
-#     fm = FastMail(conf)
-#     await fm.send_message(message, template_name='email.html')
-#
-#
-# def send_email_background(background_tasks: BackgroundTasks, subject: str, email_to: str, body: dict):
-#     message = MessageSchema(
-#         subject=subject,
-#         recipients=[email_to],
-#         body=body,
-#         subtype='html',
-#     )
-#     fm = FastMail(conf)
-#     background_tasks.add_task(
-#         fm.send_message, message, template_name='email.html')
-#
-#
+import os
+from fastapi import Request
+from fastapi import BackgroundTasks
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig, MessageType
+from fastapi.templating import Jinja2Templates
+
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+templates = Jinja2Templates(directory='templates')
+
+
+class Envs:
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_FROM = os.getenv('MAIL_FROM')
+    MAIL_PORT = int(os.getenv('MAIL_PORT'))
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_FROM_NAME = os.getenv('MAIN_FROM_NAME')
+
+
+conf = ConnectionConfig(
+    MAIL_USERNAME=Envs.MAIL_USERNAME,
+    MAIL_PASSWORD=Envs.MAIL_PASSWORD,
+    MAIL_FROM=Envs.MAIL_FROM,
+    MAIL_PORT=Envs.MAIL_PORT,
+    MAIL_SERVER=Envs.MAIL_SERVER,
+    MAIL_FROM_NAME=Envs.MAIL_FROM_NAME,
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True
+)
+
+
+def send_email_background(background_tasks: BackgroundTasks, body: dict, request: Request):
+    rendered_template = templates.TemplateResponse(
+        name="email.html",
+        context={"request": request, "form": body},
+        media_type="text/html",
+    ).body
+
+    message = MessageSchema(
+        subject='Test',
+        recipients=['sonofgod1551@gmail.com'],
+        body=rendered_template,
+        subtype=MessageType.html,
+    )
+
+    fm = FastMail(conf)
+    background_tasks.add_task(
+        fm.send_message, message, template_name=None)
