e19c0358a68bdb4cc33ac9a92d968e4391aa8cec
Demetr Sin <sonofgod1551@gmail.com>
2023-12-16 19:55:33 +0200

    almost done

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import uvicorn
+import warnings
 from fastapi import FastAPI, Request, Form, BackgroundTasks, Depends, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel, EmailStr, constr, field_validator
+from pydantic import BaseModel, EmailStr, field_validator, PositiveInt
 
-# import send_mail
+from send_mail import send_email_background
 
 app = FastAPI(debug=True)
 
@@ -18,26 +18,46 @@ templates = Jinja2Templates(directory='templates')
 class ContactForm(BaseModel):
     name: str
     email: EmailStr
-    phone: str
+    phone: PositiveInt
     message: str
 
     @classmethod
     def as_form(cls, name: str = Form(), email: EmailStr = Form(), phone: str = Form(), message: str = Form()) -> 'ContactForm': return cls(name=name, email=email, phone=phone, message=message)
 
+    @field_validator('name')
+    @classmethod
+    def name_validation(cls, value: str):
+        if not value.isalpha():
+            raise HTTPException(status_code=404, detail="Item not found")
+        if len(value) > 100:
+            raise ValueError('String length should not exceed 100 characters')
+        return value
 
-# class ResponseModel(BaseModel):
-#     message: str
-#     data: dict
+    @field_validator('message')
+    @classmethod
+    def message_validator(cls, value: str):
+        if len(value) > 1000:
+            raise ValueError("Message must contain no more then 1000 characters")
+
+
+class ResponseModel(BaseModel):
+    message: str
+    data: dict
 
 
 @app.get('/', response_class=HTMLResponse)
 async def get_home(request: Request):
-    return templates.TemplateResponse('base.html', {'request': request})
+    return templates.TemplateResponse('home.html', {'request': request})
 
 
 @app.get('/services', response_class=HTMLResponse)
 async def get_services(request: Request):
     return templates.TemplateResponse('services.html', {'request': request})
+
+
+@app.get('/representatives', response_class=HTMLResponse)
+async def get_representatives(request: Request):
+    return templates.TemplateResponse('representatives.html', {'request': request})
 
 
 @app.get('/about', response_class=HTMLResponse)
@@ -49,49 +69,9 @@ async def get_about(request: Request):
 async def get_contact(request: Request):
     return templates.TemplateResponse('contact.html', {'request': request})
 
-#Provide SMTP
+
 @app.post("/contact")
-async def post_contact(form: ContactForm = Depends(ContactForm.as_form)):
-    return form
-
-
-#simple example
-
-# @app.post('/contact')
-# async def post_contact(
-#     name: str = Form(...),
-#     email: EmailStr = Form(...),
-#     phone: str = Form(...),
-#     message: str = Form(...)
-# ):
-#     data = {
-#             "name": name,
-#             "email": email,
-#             "phone": phone,
-#             "message": message,
-#         }
-#     return {"message": "Form submitted successfully!", "data": data}
-
-
-
-
-#send
-
-
-
-# @app.get('/send-email/asynchronous')
-# async def send_email_asynchronous():
-#     await send_mail.send_email_async('Hello World','someemail@gmail.com',
-#     {'title': 'Hello World', 'name': 'John Doe'})
-#     return 'Success'
-#
-#
-# @app.get('/send-email/backgroundtasks')
-# def send_email_backgroundtasks(background_tasks: BackgroundTasks):
-#     send_mail.send_email_background(background_tasks, 'Hello World',
-#     'someemail@gmail.com', {'title': 'Hello World', 'name':       'John Doe'})
-#     return 'Success'
-#
-#
-# if __name__ == '__main__':
-#     uvicorn.run('main:app', reload=True)
+async def post_contact(background_tasks: BackgroundTasks, request: Request, form: ContactForm = Depends(ContactForm.as_form)):
+    data = form.model_dump()
+    send_email_background(background_tasks, data, request)
+    return templates.TemplateResponse('contact_response.html', {'request': request, 'form': form})
